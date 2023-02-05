9d3e328601b0f00df60932ea704ca051791fb14a
1nf0rmatix <kontakt@paul-koehler.dev>
2023-02-05 13:37:33 +0100

    adding todo

--- grid/lib/src/layout_widgets/design_grid_wrap_layout.dart
+++ grid/lib/src/layout_widgets/design_grid_wrap_layout.dart
@@ -16,6 +16,7 @@ class DesignGridWrapLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = DesignGridTheme.maybeOf(context) ?? const DesignGridThemeData();
 
+    // TODO get rid of wrap for performance reasons
     return Wrap(
       spacing: theme.columnSpacing,
       runSpacing: theme.rowSpacing,
