adc6854aaa7d75ccac73607bd4e1b0b4a40046c3
1nf0rmatix <kontakt@paul-koehler.dev>
2023-02-06 15:06:05 +0100

    adding proper example

--- grid/example/lib/main.dart
+++ grid/example/lib/main.dart
@@ -1,11 +1,84 @@
 import 'package:design_grid/design_grid.dart';
+import 'package:example/introduction_page.dart';
 import 'package:flutter/material.dart';
-
-import 'introduction_page.dart';
 
 void main() {
   runApp(const ExampleApp());
 }
+
+// TODO this will be the package example, move the talk specific stuff
+// class ExampleApp extends StatelessWidget {
+//   const ExampleApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Grid Demo',
+//       theme: ThemeData(
+//         useMaterial3: true,
+//         colorScheme: ColorScheme.fromSeed(
+//           seedColor: Colors.teal,
+//           brightness: Brightness.dark,
+//         ),
+//       ),
+//       // The DesignGridConfig should be the top most widget.
+//       home: DesignGridConfig(
+//         child: Scaffold(
+//           body: DesignGrid(
+//             // You can specify the alignment of the grid, default is start.
+//             alignment: DesignGridAlignment.center,
+//             children: [
+//               // This will wrap its children if necessary.
+//               // If you want to enforce a break, use another DesignGridRow.
+//               // For special use-cases you can also use DesignGridItemBreak to force a break.
+//               DesignGridRow(
+//                 // You can specify the alignment of the row, default is start.
+//                 alignment: DesignGridRowAlignment.center,
+//                 children: [
+//                   DesignGridItem(
+//                     // Specify the number of columns for each display size.
+//                     // Small is always required, all others will be inherited in order with the breakpoints:
+//                     // small, medium, large, extraLarge
+//                     columns: const DesignGridColumns(small: 12),
+//                     // The child will be constrained in width according to the number of columns.
+//                     child: Container(
+//                       padding: const EdgeInsets.all(16),
+//                       color: Colors.grey,
+//                       child: const Text('I span 12 columns'),
+//                     ),
+//                   ),
+//                   DesignGridItem(
+//                     columns: const DesignGridColumns(small: 6),
+//                     child: Container(
+//                       padding: const EdgeInsets.all(16),
+//                       color: Colors.grey,
+//                       child: const Text('I span 6 columns'),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               DesignGridRow(
+//                 children: [
+//                   DesignGridItem(
+//                     columns: const DesignGridColumns(
+//                       small: 8,
+//                       large: 4,
+//                     ),
+//                     child: Container(
+//                       padding: const EdgeInsets.all(16),
+//                       color: Colors.grey,
+//                       child: const Text('I span 8 columns on small screens and 4 on large screens'),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class ExampleApp extends StatelessWidget {
   const ExampleApp({super.key});
--- grid/lib/design_grid.dart
+++ grid/lib/design_grid.dart
@@ -24,8 +24,6 @@ export 'src/widgets/widgets.dart';
 /// The [DesignGrid] widget will calculate the layout based on the current [DesignGridDisplaySize] and the [DesignGridTheme].
 /// You can use the [DesignGridItem] widget to define the number of columns for a child, depending on the display size.
 ///
-/// [DesignGridItemBreak] widget will define a break in the grid, i.e. a new row.
-///
 /// Alternatively you can use multiple [DesignGrid] in a [Column] or [ListView]. This will also greatly improve performance in [ListView].
 ///
 /// Nesting grids is possible by using the [DesignGrid] widget inside a [DesignGridItem].
@@ -34,8 +32,80 @@ export 'src/widgets/widgets.dart';
 /// If you use the [DesignGrid] inside a child but the [DesignGrid] itself doesn't get the full width, i.e. inside a card or a setup with other widgets, you can set [DesignGrid.shouldCalculateLayout] to true.
 ///
 /// Example:
-/// TODO: Add example
-/// TODO maybe rename to ResponsiveDesignGrid or ResponsiveGrid (package name however should stay design_grid)
+/// ```dart
+///class ExampleApp extends StatelessWidget {
+///  const ExampleApp({super.key});
+///
+///  @override
+///  Widget build(BuildContext context) {
+///    return MaterialApp(
+///      title: 'Grid Demo',
+///      theme: ThemeData(
+///        useMaterial3: true,
+///        colorScheme: ColorScheme.fromSeed(
+///          seedColor: Colors.teal,
+///          brightness: Brightness.dark,
+///        ),
+///      ),
+///      // The DesignGridConfig should be the top most widget.
+///      home: DesignGridConfig(
+///        child: Scaffold(
+///          body: DesignGrid(
+///            // You can specify the alignment of the grid, default is start.
+///            alignment: DesignGridAlignment.center,
+///            children: [
+///              // This will wrap its children if necessary.
+///              // If you want to enforce a break, use another DesignGridRow.
+///              // For special use-cases you can also use DesignGridItemBreak to force a break.
+///              DesignGridRow(
+///                // You can specify the alignment of the row, default is start.
+///                alignment: DesignGridRowAlignment.center,
+///                children: [
+///                  DesignGridItem(
+///                    // Specify the number of columns for each display size.
+///                    // Small is always required, all others will be inherited in order with the breakpoints:
+///                    // small, medium, large, extraLarge
+///                    columns: const DesignGridColumns(small: 12),
+///                    // The child will be constrained in width according to the number of columns.
+///                    child: Container(
+///                      padding: const EdgeInsets.all(16),
+///                      color: Colors.grey,
+///                      child: const Text('I span 12 columns'),
+///                    ),
+///                  ),
+///                  DesignGridItem(
+///                    columns: const DesignGridColumns(small: 6),
+///                    child: Container(
+///                      padding: const EdgeInsets.all(16),
+///                      color: Colors.grey,
+///                      child: const Text('I span 6 columns'),
+///                    ),
+///                  ),
+///                ],
+///              ),
+///              DesignGridRow(
+///                children: [
+///                  DesignGridItem(
+///                    columns: const DesignGridColumns(
+///                      small: 8,
+///                      large: 4,
+///                    ),
+///                    child: Container(
+///                      padding: const EdgeInsets.all(16),
+///                      color: Colors.grey,
+///                      child: const Text('I span 8 columns on small screens and 4 on large screens'),
+///                    ),
+///                  ),
+///                ],
+///              ),
+///            ],
+///          ),
+///        ),
+///      ),
+///    );
+///  }
+///}
+/// ```
 class DesignGrid extends StatelessWidget {
   final DesignGridAlignment alignment;
 
